3b0069a904ad3a173e7f61d25bfa8be0929ea9f4
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-04-06 19:27:32 -0700

    add member display functionallity on index.component

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from '../member.model';
+import { MemberService } from '../member.service';
+import { Router } from '@angular/router';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.css']
+  styleUrls: ['./index.component.css'],
+  providers: [MemberService]
 })
 export class IndexComponent implements OnInit {
 
-  constructor() { }
+  members: FirebaseListObservable<any[]>;
+
+  constructor(private memberService: MemberService) { }
 
   ngOnInit() {
+    this.members = this.memberService.getMembers();
   }
 
 }
--- src/app/member-detail/member-detail.component.ts
+++ src/app/member-detail/member-detail.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { Member } from '../member.model';
+import { MemberService } from '../member.service';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-member-detail',
   templateUrl: './member-detail.component.html',
-  styleUrls: ['./member-detail.component.css']
+  styleUrls: ['./member-detail.component.css'],
+  providers: [MemberService]
 })
 export class MemberDetailComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
