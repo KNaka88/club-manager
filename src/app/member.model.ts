9fcbb6eadd837ed2141645406f2232a1010b6055
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-04-06 18:57:41 -0700

    add sample-members.json

--- src/app/member.model.ts
+++ src/app/member.model.ts
@@ -1,6 +1,7 @@
 export class Member {
   constructor (
     public name: string,
+    public number: number;
     public picUrl: string,
     public description: string,
     public position: string,
