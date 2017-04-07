7a1f9db42cef74dd0724fdf5ff6601c3c900d905
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-04-06 18:58:21 -0700

    change class member's datatype

--- src/app/member.model.ts
+++ src/app/member.model.ts
@@ -1,7 +1,7 @@
 export class Member {
   constructor (
     public name: string,
-    public number: number;
+    public number: string;
     public picUrl: string,
     public description: string,
     public position: string,
